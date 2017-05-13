c6225fb9d1a5f819638cbddd1e8a6d19e72fd9d6
Ian F <ian.fennie@gmail.com>
2017-05-13 23:04:53 +0200

    Initialize room exits with .setExit()

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Set;
 
 /**
@@ -46,11 +47,26 @@ public class Game
         office = new Room("in the computing admin office");
         
         // initialise room exits
-        outside.setExits(null, theater, lab, pub);
-        theater.setExits(null, null, null, outside);
-        pub.setExits(null, outside, null, null);
-        lab.setExits(outside, office, null, null);
-        office.setExits(null, null, null, lab);
+        outside.setExit("north",null);
+        outside.setExit("east", theater);
+        outside.setExit("south", lab);
+        outside.setExit("west", pub);
+        theater.setExit("north", null);
+        theater.setExit("east", null);
+        theater.setExit("south", null);
+        theater.setExit("west", outside);
+        pub.setExit("north", null);
+        pub.setExit("east", outside);
+        pub.setExit("south", null);
+        pub.setExit("west", null);
+        lab.setExit("north", outside);
+        lab.setExit("east", office);
+        lab.setExit("south",null);
+        lab.setExit("west", null);
+        office.setExit("north",null);
+        office.setExit("east", null);
+        office.setExit("south",null);
+        office.setExit("west", lab);
 
         currentRoom = outside;  // start game outside
     }
